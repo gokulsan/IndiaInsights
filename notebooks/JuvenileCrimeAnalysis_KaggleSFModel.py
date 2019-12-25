799c3cbce422859aeb73561507fb6500586c526b
Gokul Alex <gokulsan@users.noreply.github.com>
2019-12-25 13:16:09 +0530

    Data Partitioning and Normalisation
    Random Data Partitioning and Attempting Data Normalisation

--- notebooks/JuvenileCrimeAnalysis_KaggleSFModel.py
+++ notebooks/JuvenileCrimeAnalysis_KaggleSFModel.py
@@ -13,6 +13,7 @@ from shapely.geometry import Point, Polygon, shape
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
+# Reading file from the filesystem
 import os
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
@@ -24,24 +25,19 @@ warnings.filterwarnings("ignore")
 sns.set(style = 'darkgrid')
 sns.set_palette('PuBuGn_d')
 
-train = pd.read_csv("../input/indiainsights-juvenilecrimes-1995-2005/IndiaGov_JuvenileCrimes_1995_2005.csv")
-train.head(9)
-train.shape
-train.isnull().sum()
+df_total = pd.read_csv("../input/indiainsights-juvenilecrimes-1995-2005/IndiaGov_JuvenileCrimes_1995_2005.csv")
+df_total.head(9)
+df_total.iloc[:, 4].values
 
-x = sns.catplot('Incidence of Juvenile Crimes', data = train, kind = 'count', aspect = 3, height = 4.5)
-x.set_xticklabels(rotation = 85)
+# Random Partitioning of the Dataset using Scikit Learn Train Test Splitter
+X, Y = df_total.iloc[:, 0:].values, df_total.iloc[:, 1:].values, 
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.2, random_state=0)
 
-le = preprocessing.LabelEncoder()
-jCrimes = le.fit_transform(train['Incidence of Juvenile Crimes'])
+# Measuring the length of the Training and Test Dataset
+len (X_train), len(X_test)
 
-training, validation = train_test_split(jCrimes, train_size = 0.60)
-
-start = time.time()
-model = BernoulliNB()
-model.fit(training[features], training['jCrimes'])
-predicted = np.array(model.predict_proba(validation[features]))
-end = time.time()
-secs = (end - start)
-loss = log_loss(validation['jCrimes'], predicted)
-print("Total seconds: {} and loss {}".format(secs, loss))
+# Attempting Data Normalisation using MinMax Scaler
+from sklearn.preprocessing import MinMaxScaler
+mms = MinMaxScaler()
+X_train_norm = mms.fit_transform(X_train)
+X_test_norm = mms.fit_transform(X_test)
